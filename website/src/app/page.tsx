e7d27a076b1c689f3ab83f3655cdb3ecb7f1f644
Carmen Mueller <152515728+Tatermater04@users.noreply.github.com>
2025-03-13 12:38:31 +0000

    Downloaded MUI Nextjs template

---
+++ website/src/app/page.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import NextLink from 'next/link';
+import ProTip from '@/components/ProTip';
+import Copyright from '@/components/Copyright';
+import Button from '@mui/material/Button';
+import Hero from '@/components/Hero';
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+      
+      <Hero />
+      </Box>
+    </Container>
+  );
+}
