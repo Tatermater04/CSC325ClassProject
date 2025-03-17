a5f629128429ac23dd9c65efee4cfc858f49300f
Carmen Mueller <152515728+Tatermater04@users.noreply.github.com>
2025-03-17 22:37:29 +0000

    added public/static/images/avatars directory with images

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
@@ -8,6 +9,16 @@ import ProTip from '@/components/ProTip';
 import Copyright from '@/components/Copyright';
 import Button from '@mui/material/Button';
 import Hero from '@/components/Hero';
+import LogoCollection from '@/components/LogoCollection';
+import Features from '@/components/Features';
+import Pricing from '@/components/Pricing';
+import FAQ from '@/components/FAQ';
+import Footer from '@/components/Footer';
+
+
+import Highlights from '@/components/Highlights';
+import Testimonials from '@/components/Testimonials';
+import { Divider } from '@mui/material';
 
 export default function Home() {
   return (
@@ -22,7 +33,22 @@ export default function Home() {
         }}
       >
       
-      <Hero />
+        <Hero />
+        <div>
+          <LogoCollection />
+          <Features />
+          <Divider />
+          <Testimonials />
+          <Divider />
+          <Highlights />
+          <Divider />
+          <Pricing />
+          <Divider />
+          <FAQ />
+          <Divider />
+          <Footer />
+        </div>
+
       </Box>
     </Container>
   );
