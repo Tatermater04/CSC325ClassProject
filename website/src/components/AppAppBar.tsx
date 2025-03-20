c9df216752d3a6aef81bb3df5f6169c46dd07b07
Carmen Mueller <152515728+Tatermater04@users.noreply.github.com>
2025-03-20 12:13:05 +0000

    Added documentation

--- website/src/components/AppAppBar.tsx
+++ website/src/components/AppAppBar.tsx
@@ -73,6 +73,9 @@ export default function AppAppBar() {
               <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
                 Blog
               </Button>
+              <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }} href='/about'>
+                About
+              </Button>
             </Box>
           </Box>
           <Box
